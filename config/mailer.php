69611c2dfd2c9374e8536bf738275e0bc5b195ef
Wnoski <161647207+Wnoski@users.noreply.github.com>
2026-03-07 14:10:02 +0100

    Update mailer password variable name

--- config/mailer.php
+++ config/mailer.php
@@ -11,7 +11,7 @@ function crearMailer() {
     $mail->Host       = 'el_host';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'tu_username_o_del_gestor_de_correos';
-    $mail->Password   = 'tu_api_key';
+    $mail->Password   = 'tu_password_o_api_key';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL en puerto 465
     $mail->Port       = 465;
     $mail->CharSet    = 'UTF-8';
