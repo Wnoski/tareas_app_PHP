ca7d89e8b28f1f998a2132abb7ddc2d8fd880ba8
Wnoski <161647207+Wnoski@users.noreply.github.com>
2026-03-07 14:08:17 +0100

    Cambios en el Mailer

--- config/mailer.php
+++ config/mailer.php
@@ -8,17 +8,16 @@ function crearMailer() {
     $mail = new PHPMailer(true); // true activa las excepciones
 
     $mail->isSMTP();
-    $mail->Host       = 'smtp.resend.com';
+    $mail->Host       = 'el_host';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'resend';
-    $mail->Password   = 're_cwm9wE6p_7BqZ62ptyBFj5vJ2ya7BKuCY';
+    $mail->Username   = 'tu_username_o_del_gestor_de_correos';
+    $mail->Password   = 'tu_api_key';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL en puerto 465
     $mail->Port       = 465;
     $mail->CharSet    = 'UTF-8';
 
-    // Remitente — debe ser un dominio verificado en Resend
-    // Para pruebas Resend permite onboarding@resend.dev
-    $mail->setFrom('onboarding@resend.dev', 'Tareas App');
+    
+    $mail->setFrom('correo_remitente', 'Tareas App');
 
     return $mail;
 }
